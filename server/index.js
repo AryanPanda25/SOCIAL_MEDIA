502e37429eb83b1520fc3d9da7b999b1029024d8
AryanPanda <aryanpanda100@gmail.com>
2023-09-08 14:59:30 +0530

    new commit

--- server/index.js
+++ server/index.js
@@ -42,9 +42,9 @@ cloudinary.config({
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
-  // destination: function (req, file, cb) {
-  //   cb(null, "public/assets");
-  // },
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   }
